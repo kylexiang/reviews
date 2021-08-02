b06dc0111e670d77253ea2fc9b135bfe26333bec
kylexiang <87591545+kylexiang@users.noreply.github.com>
2021-08-02 09:57:33 +0800

    Update read.py

--- read.py
+++ read.py
@@ -6,10 +6,18 @@ with open ("read(data)","r") as file:
 		count += 1;
 		if count % 1000 == 0:
 			print(len(data));
+
 for i in data:
 	print(i,"\n--------------------------------");
+
 a = 0;
 for i in data:
 	a += len(i);
 print("资料总长度为",a);
 print("资料平均长度为",(a / len(data)));
+
+b = [];
+for k in data:
+	if len(k) < 100:
+		b.append(k);
+print("一共有", len(b), "个小于100");
